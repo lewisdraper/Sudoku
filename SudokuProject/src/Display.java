cbd7bc6e064e6c4703174f4d61e785244bd0f578
Lewis <Lewis@Lewis-Desktop.Home>
2018-02-08 14:37:36 +0000

    displayclass

---
+++ SudokuProject/src/Display.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+
+public class Display {
+
+	private JFrame window;
+	private int[][] grid;
+	
+
+	public Display(int[][] grid){
+		this.grid = grid;
+		createDisplay();
+	}
+	
+	private void createDisplay(){
+		
+		
+		
+	}
+	
+}
